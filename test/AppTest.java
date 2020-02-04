c887ee8e586ac4e8909ea16fdc064d3cb644af61
Jayashree <Jayashree.r@sonata-software.com>
2020-02-04 08:56:37 +0530

    for test

---
+++ test/AppTest.java
@@ -0,0 +1,16 @@
+import junit.framework.*;
+
+public class AppTest extends TestCase {
+    public void test1() {
+    }
+
+    public void test2() {
+        fail("test failed!");
+    }
+	
+	 public void test3() {
+        App app = new App();
+		//assertEqual(4,app.add(2,2));
+    }
+	
+}
